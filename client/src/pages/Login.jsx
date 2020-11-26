45515e7869eaa79d934d004a11bf0c56e090d4fc
Gustavo nRm <gustavonrm@gmail.com>
2020-11-26 18:58:54 +0000

    pseudo pagina de login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import api from "../api";
+import { Logo } from "../components";
+import "../style/Login.css";
 
 class Login extends Component {
   constructor(props) {
@@ -8,7 +10,40 @@ class Login extends Component {
     this.state = {};
   }
   render() {
-    return <>Login</>;
+    return (
+      <>
+        <Logo />
+
+        <form class="form-signin">
+          <div class="text-center mb-4"></div>
+
+          <div class="form-label-group">
+            <input
+              type="email"
+              id="inputEmail"
+              class="form-control"
+              placeholder="Email address"
+              required=""
+              autofocus=""
+            />
+          </div>
+
+          <div class="form-label-group">
+            <input
+              type="password"
+              id="inputPassword"
+              class="form-control"
+              placeholder="Password"
+              required=""
+            />
+          </div>
+
+          <button class="btn btn-lg btn-primary btn-block" type="submit">
+            Sign in
+          </button>
+        </form>
+      </>
+    );
   }
 }
 
