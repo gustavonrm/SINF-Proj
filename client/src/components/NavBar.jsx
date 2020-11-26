783a7282b0a568da9e1b3353467a6ba484afc136
Gustavo nRm <gustavonrm@gmail.com>
2020-11-26 17:34:36 +0000

    navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+
+import "../style/NavBar.css";
 
 class NavBar extends Component {
   state = {};
   render() {
     return (
       <>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrapshadow">
-          <div style={{ color: "white", zIndex: "-1" }}>360º DashBoard</div>
+        <nav className="navbar fixed-top flex-md-nowrapshadow">
+          <div>360º DashBoard</div>
           <ul class="navbar-nav px-3">
             <li className="nav-item text-nowrap">
               <a className="nav-link" href="#">
-                Sign out
+                Logout
+                <FontAwesomeIcon icon={faSignOutAlt} className="ml-2" />
               </a>
             </li>
           </ul>
