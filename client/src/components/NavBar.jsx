1bccf7f964b98b4ce10fa1e93b3371dc872a5bdf
Gustavo nRm <gustavonrm@gmail.com>
2020-11-26 17:21:20 +0000

    muita coisa

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,16 +5,8 @@ class NavBar extends Component {
   render() {
     return (
       <>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
-            Company name
-          </a>
-          <input
-            className="form-control form-control-dark w-100"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          />
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrapshadow">
+          <div style={{ color: "white", zIndex: "-1" }}>360º DashBoard</div>
           <ul class="navbar-nav px-3">
             <li className="nav-item text-nowrap">
               <a className="nav-link" href="#">
--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -13,7 +13,250 @@ class Overview extends Component {
     return (
       <>
         <NavBar />
-        <SideNav />
+        <div className="container-fluid">
+          <div className="row">
+            <SideNav />
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+              <div
+                style={{
+                  position: "absolute",
+                  inset: "0px",
+                  overflow: "hidden",
+                  pointerEvents: "none",
+                  visibility: "hidden",
+                  zIndex: "-1",
+                }}
+                className="chartjs-size-monitor"
+              >
+                <div
+                  className="chartjs-size-monitor-expand"
+                  style={{
+                    position: "absolute",
+                    left: "0",
+                    top: "0",
+                    right: "0",
+                    bottom: "0",
+                    overflow: "hidden",
+                    pointerEvents: "none",
+                    visibility: "hidden",
+                    zIndex: "-1",
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "absolute",
+                      width: "1000000px",
+                      height: "1000000px",
+                      left: "0",
+                      top: "0",
+                    }}
+                  ></div>
+                </div>
+                <div
+                  className="chartjs-size-monitor-shrink"
+                  style={{
+                    position: "absolute",
+                    left: "0",
+                    top: "0",
+                    right: "0",
+                    bottom: "0",
+                    overflow: "hidden",
+                    pointerEvents: "none",
+                    visibility: "hidden",
+                    zIndex: "-1",
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "absolute",
+                      width: "200%",
+                      height: "200%",
+                      left: "0",
+                      top: "0",
+                    }}
+                  ></div>
+                </div>
+              </div>
+              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Dashboard</h1>
+                <div className="btn-toolbar mb-2 mb-md-0">
+                  <div className="btn-group mr-2">
+                    <button className="btn btn-sm btn-outline-secondary">
+                      Share
+                    </button>
+                    <button className="btn btn-sm btn-outline-secondary">
+                      Export
+                    </button>
+                  </div>
+                  <button className="btn btn-sm btn-outline-secondary dropdown-toggle">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="feather feather-calendar"
+                    >
+                      <rect
+                        x="3"
+                        y="4"
+                        width="18"
+                        height="18"
+                        rx="2"
+                        ry="2"
+                      ></rect>
+                      <line x1="16" y1="2" x2="16" y2="6"></line>
+                      <line x1="8" y1="2" x2="8" y2="6"></line>
+                      <line x1="3" y1="10" x2="21" y2="10"></line>
+                    </svg>
+                    This week
+                  </button>
+                </div>
+              </div>
+
+              <canvas
+                className="my-4 w-100 chartjs-render-monitor"
+                id="myChart"
+                width="1540"
+                height="650"
+                style={{ display: "block", width: "1540px", height: "650px" }}
+              ></canvas>
+
+              <h2>Section title</h2>
+              <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1,001</td>
+                      <td>Lorem</td>
+                      <td>ipsum</td>
+                      <td>dolor</td>
+                      <td>sit</td>
+                    </tr>
+                    <tr>
+                      <td>1,002</td>
+                      <td>amet</td>
+                      <td>consectetur</td>
+                      <td>adipiscing</td>
+                      <td>elit</td>
+                    </tr>
+                    <tr>
+                      <td>1,003</td>
+                      <td>Integer</td>
+                      <td>nec</td>
+                      <td>odio</td>
+                      <td>Praesent</td>
+                    </tr>
+                    <tr>
+                      <td>1,003</td>
+                      <td>libero</td>
+                      <td>Sed</td>
+                      <td>cursus</td>
+                      <td>ante</td>
+                    </tr>
+                    <tr>
+                      <td>1,004</td>
+                      <td>dapibus</td>
+                      <td>diam</td>
+                      <td>Sed</td>
+                      <td>nisi</td>
+                    </tr>
+                    <tr>
+                      <td>1,005</td>
+                      <td>Nulla</td>
+                      <td>quis</td>
+                      <td>sem</td>
+                      <td>at</td>
+                    </tr>
+                    <tr>
+                      <td>1,006</td>
+                      <td>nibh</td>
+                      <td>elementum</td>
+                      <td>imperdiet</td>
+                      <td>Duis</td>
+                    </tr>
+                    <tr>
+                      <td>1,007</td>
+                      <td>sagittis</td>
+                      <td>ipsum</td>
+                      <td>Praesent</td>
+                      <td>mauris</td>
+                    </tr>
+                    <tr>
+                      <td>1,008</td>
+                      <td>Fusce</td>
+                      <td>nec</td>
+                      <td>tellus</td>
+                      <td>sed</td>
+                    </tr>
+                    <tr>
+                      <td>1,009</td>
+                      <td>augue</td>
+                      <td>semper</td>
+                      <td>porta</td>
+                      <td>Mauris</td>
+                    </tr>
+                    <tr>
+                      <td>1,010</td>
+                      <td>massa</td>
+                      <td>Vestibulum</td>
+                      <td>lacinia</td>
+                      <td>arcu</td>
+                    </tr>
+                    <tr>
+                      <td>1,011</td>
+                      <td>eget</td>
+                      <td>nulla</td>
+                      <td>Class</td>
+                      <td>aptent</td>
+                    </tr>
+                    <tr>
+                      <td>1,012</td>
+                      <td>taciti</td>
+                      <td>sociosqu</td>
+                      <td>ad</td>
+                      <td>litora</td>
+                    </tr>
+                    <tr>
+                      <td>1,013</td>
+                      <td>torquent</td>
+                      <td>per</td>
+                      <td>conubia</td>
+                      <td>nostra</td>
+                    </tr>
+                    <tr>
+                      <td>1,014</td>
+                      <td>per</td>
+                      <td>inceptos</td>
+                      <td>himenaeos</td>
+                      <td>Curabitur</td>
+                    </tr>
+                    <tr>
+                      <td>1,015</td>
+                      <td>sodales</td>
+                      <td>ligula</td>
+                      <td>in</td>
+                      <td>libero</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </main>
+          </div>
+        </div>
       </>
     );
   }
