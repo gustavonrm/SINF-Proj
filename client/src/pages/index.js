71abfc2fae546840a70ebe5e9eb03a3418a38fe1
tiagogsilva <tiago.silva.99@hotmail.com>
2020-12-06 20:44:01 +0000

    First version of accounts' components

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,5 +5,6 @@ import Purchases from './Purchases'
 import Financial from './Financial'
 import Inventory from './Inventory'
 import Accounts from './Accounts'
+import WIP from './WIP'
 
 export { Login, Overview, Sales, Purchases, Financial, Inventory, Accounts}
