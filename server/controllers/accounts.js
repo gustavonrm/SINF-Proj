2fa03feb81dc0a97b2be533a87f02574d4ac3975
Pastilhas <joao.campos1221@gmail.com>
2020-12-07 19:26:04 +0000

    troll

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -17,8 +17,59 @@ Controller.accountsReceivable = (req, res) => {
     });
 };
 
-Controller.receivableGraph = (req, res) => {
-  //TODO: receivableGraph graph
+Controller.receivableTable = (req, res) => {
+  const response = [];
+
+  jasminReq("get", "/invoiceReceipt/invoices")
+    .then((data) => {
+      data.forEach((invoice) => {
+        if (invoice.cashInvoice) continue;
+        const supplier =
+          invoice.sellerSupplierPartyName || invoice.sellerSupplierParty;
+
+        invoice.documentLines.forEach((item) => {
+          const getKey = (item) => item.itemKey;
+          const getName = (item) => item.purchasesItem;
+          const getDueDate = (item) => getTimestamp(item.dueDate);
+          const getQuantity = (item) =>
+            item.materialsItemWarehouses.reduce(
+              (accumulator, currValue) => accumulator + currValue.stockBalance
+            );
+          const getUnitCost = (item) =>
+            item.materialsItemWarehouses.reduce(
+              (accumulator, currValue) =>
+                accumulator + currValue.calculatedUnitCost
+            ) / item.materialsItemWarehouses.length;
+
+          const key = getKey(item);
+          const name = getName(item);
+          const dueDate = getDueDate(item);
+          const quantity = getQuantity(item);
+          const unitCost = getUnitCost(item);
+          const totalCost = quantity * unitCost;
+
+          response.push({
+            key: key,
+            name: name,
+            supplier: supplier,
+            dueDate: dueDate,
+            quantity: quantity,
+            unitCost: unitCost,
+            totalCost: totalCost,
+          });
+        });
+      });
+
+      res.json(response);
+    })
+    .catch(() => {
+      const err = new Error("Failed");
+      err.status = 400;
+      res.status(400).json({
+        message: err.message,
+        error: err,
+      });
+    });
 };
 
 Controller.accountsPayable = (req, res) => {
@@ -36,7 +87,7 @@ Controller.accountsPayable = (req, res) => {
     });
 };
 
-Controller.payableGraph = (req, res) => {
+Controller.payableTable = (req, res) => {
   //TODO: payableGraph graph
 };
 
