28317f4fc3ffd087a05bb437e21e7a980059bb4a
Gustavo nRm <gustavonrm@gmail.com>
2020-12-08 18:47:49 +0000

    n tem mt a ver mas e db remota

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const { connect, connection } = require("mongoose");
 
-connect("mongodb://127.0.0.1:27017/sinf", { useNewUrlParser: true }).catch(
+//connect("mongodb://127.0.0.1:27017/sinf", { useNewUrlParser: true }).catch(
+  connect("mongodb+srv://admin:1234@cluster0.um73a.mongodb.net/sinf?retryWrites=true&w=majority",{ useNewUrlParser: true }).catch(
   (e) => {
     console.error("Connection error", e.message);
   }
