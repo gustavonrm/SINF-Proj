38906261c51142c8c5263553c94fc85322c989ac
Gustavo nRm <gustavonrm@gmail.com>
2020-12-12 16:56:24 +0000

    errors

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 const ctrl = require('../controllers/accounts');
 const router = Router();
 
-router.get('/accountsReceivable', ctrl.accountsReceivable);
-router.get('/receivableTable', ctrl.receivableTable);
-router.get('/accountsPayable', ctrl.accountsPayable);
+router.get('/accountsReceivable', ctrl.accountsReceivable); //fix
+router.get('/receivableTable', ctrl.receivableTable); //fix
+router.get('/accountsPayable', ctrl.accountsPayable); //fix
 router.get('/payableTable', ctrl.payableTable);  
 
 module.exports = router;
