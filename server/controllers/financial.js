ee8776daa7696e2b4aba7304cf24a717d16aacf3
BrunoFCM <bruno_micaelo@hotmail.com>
2020-12-16 00:33:00 +0000

    Added route to the balance sheet

--- server/controllers/financial.js
+++ server/controllers/financial.js
@@ -62,4 +62,19 @@ Controller.growthRatios = (req, res) => {
     });
 };
 
+Controller.growthRatios = (req, res) => {
+  saftReq('/balanceSheet')
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(() => {
+      const err = new Error('Failed');
+      err.status = 400;
+      res.status(400).json({
+        message: err.message,
+        error: err,
+      });
+    });
+};
+
 module.exports = Controller;
